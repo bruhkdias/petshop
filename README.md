29f83b617ada1320b46f6651fb5724e1ae79e4ad
bruhkdias <brunaketlyn.13@gmail.com>
2021-03-22 21:31:46 -0300

    Adicionando projeto no github

---
+++ README.md
@@ -0,0 +1,11 @@
+### Layout de Petshop
+
+**Projeto criado em grupo, durante o período da graduação de Análise e Desenvolvimento de Sistemas.**
+
+**Utilizamos:**
+
+* HTML5 
+* Javascript 
+* CSS
+* Jquery v3.3.1
+* Bootstrap v3.3.7
---
+++ js/petcoder.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".seta").click(function(){
+        $(".login").slideToggle("slow");
+    });
+});
+
+
+function enviado(){
+   alert("Enviado Com Sucesso")
+}
+
+function cadastro(){
+    alert("Cadastrado Com Sucesso")
+}
